e3a0b3e544e8dd0ce552d47f438c81c7bdcfa2b9
Frederick Doar <rickydoar@gmail.com>
2025-12-17 15:38:11 -0800

    Update hero button: Change 'View Listings' to 'Home Search' linking to IDX search

--- app/page.tsx
+++ app/page.tsx
@@ -32,12 +32,12 @@ export default function Home() {
             Your trusted real estate partner in Marin County
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <Link
-              href="/portfolio/active"
+            <a
+              href="https://ninabelloni.com/search/#status=active&sold_days=180&pstatus=1%2C11&ls_conversion=acres&location_search_field=Marin%20County%2C%20CA&drive_time=09%3A00&drive_duration=15&drive_avoid_ferry=1&drive_departure=1&ss_locale=en-US&ss_description=Marin%20County%2C%20CA&ss_email_freq=40&ss_send_zero_result=1&bounds_north=38.8157695277121&bounds_east=-122.05984965225001&bounds_south=37.31297310571092&bounds_west=-123.42215433975001&center_lat=38.083403&center_lon=-122.7633036&center_lat_pan=38.0682300569306&center_lon_pan=-122.74100199600001&geotype=AdminDivision2&user_lat=38.083403&user_lon=-122.7633036&pgsize=20&startidx=0&zoom=9&sort_by=1&company_uuid=4221721&commute=0&buffer_miles=0.25&geospatial=true&agent_uuid=2ef5a310-9e45-494d-874b-33da57cab703&ptype=1%2C2%2C9&searchType=criteria&omit_hidden=true&ex_pend=true&currency=USD"
               className="px-8 py-3 bg-white text-gray-900 font-medium hover:bg-gray-100 transition-colors rounded-sm"
             >
-              View Listings
-            </Link>
+              Home Search
+            </a>
             <Link
               href="/contact"
               className="px-8 py-3 border-2 border-white text-white font-medium hover:bg-white/10 transition-colors rounded-sm"
